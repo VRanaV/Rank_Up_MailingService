55c8f20489f01d8ade977919dc06772e8314fda1
VRanaV <41893299+VRanaV@users.noreply.github.com>
2020-08-20 03:00:23 +0200

    service

---
+++ config/default.js
@@ -0,0 +1,23 @@
+'use strict';
+require('dotenv').config();
+module.exports = {
+    app: {
+        name: "MailingService",
+        baseUrl: `http://localhost:`,
+        port: process.env.PORT || 3006
+    },
+    client: {
+        url: process.env.CLIENT || 'http://localhost:3000'
+    },
+    api: {
+        prefix: '^/api/v[1-9]',
+        version: [1],
+    },
+    database: {
+        url: process.env.DB_URL || "mongodb+srv://rankup:rank1998@cluster0-9enme.mongodb.net/RUMails?retryWrites=true&w=majority",
+    }, 
+    cred : {
+        user: 'rankup9822@yahoo.com',
+        pass: 'mpvyeqpdvqlmwncq'
+    }
+};
